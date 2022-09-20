af06456787a108c173f54f473e56b0b9f5b5867b
ghouseshaikk <98320637+ghouseshaikk@users.noreply.github.com>
2022-09-20 17:18:57 +0530

    Add files via upload

---
+++ ekartAPI/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
+
+
+mongoose
+  .connect(
+  "mongodb+srv://Ghouse:Fellowm@cluster0.0jpzlar.mongodb.net/?retryWrites=true&w=majority"
+  )
+
+  .then (() => console.log("DBConnection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+app.use(cors());
+app.use(express.json());  
+app.use("/api/auth", authRoute); 
+app.use("/api/users", userRoute); 
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
+
+
+  app.listen(5000, () => {
+    console.log("Backend server is running!");
+  });
